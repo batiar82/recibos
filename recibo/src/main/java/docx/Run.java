79b822888266b64fc3878fc2953b0c6cfa50a571
Mariano Moreno Alvariza <mmoreno@ferrobaires.gba.gov.ar>
2017-07-25 18:09:18 -0300

    App ejemplo

---
+++ recibo/src/main/java/docx/Run.java
@@ -0,0 +1,15 @@
+package docx;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
